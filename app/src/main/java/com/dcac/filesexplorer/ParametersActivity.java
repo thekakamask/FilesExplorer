ccb39c23391ca56784d63210a6ccb2396ee1ff9a
Antoine de Chaunac <adechaunac@hotmail.fr>
2023-11-28 20:39:48 +0100

    ajout de la fonctionnalité de parametre pour le changement de theme de l'application. le changement de theme fonctionne (en tt cas tant que l'utilisateur est sur la page des parametres) il y'a tjr un probleme avec les cases des themes correspondant a cocher qui ne s'affiche pas bien.)

--- app/src/main/java/com/dcac/filesexplorer/ParametersActivity.java
+++ app/src/main/java/com/dcac/filesexplorer/ParametersActivity.java
@@ -2,14 +2,16 @@ package com.dcac.filesexplorer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ExpandableListView;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
-public class ParametersActivity extends AppCompatActivity {
+public class ParametersActivity extends AppCompatActivity implements ParametersActivityExpandableListAdapter.OnThemeChangeListener  {
 
     private ExpandableListView expandableListView;
     private ParametersActivityExpandableListAdapter parametersActivityExpandableListAdapter;
@@ -18,13 +20,16 @@ public class ParametersActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // Appliquer le thème sélectionné
+        applySelectedTheme();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_parameters);
+
 
         prepareListData(); // Préparez vos données ici
 
         expandableListView = findViewById(R.id.expandableListView);
-        parametersActivityExpandableListAdapter = new ParametersActivityExpandableListAdapter(this, listGroupTitles, listChildData);
+        parametersActivityExpandableListAdapter = new ParametersActivityExpandableListAdapter(this, listGroupTitles, listChildData, this);
         expandableListView.setAdapter(parametersActivityExpandableListAdapter);
 
         // Gestion des clics sur les groupes et les enfants, si nécessaire
@@ -43,8 +48,8 @@ public class ParametersActivity extends AppCompatActivity {
         listGroupTitles.add("Global title example 2");
         listGroupTitles.add("Global title example 3");
 
-        themes.add(new ParametersItem("Base theme", "this theme is the basic theme", false));
-        themes.add(new ParametersItem("Dark theme", "this theme is for the day", false));
+        themes.add(new ParametersItem("Base theme", "this theme is the basic theme", true));
+        themes.add(new ParametersItem("Light theme", "this theme is for the day", false));
         themes.add(new ParametersItem("Dark theme", "this theme is for the night", false));
 
         userCommands.add(new ParametersItem("Title example 1", "Description example 1", false));
@@ -60,9 +65,36 @@ public class ParametersActivity extends AppCompatActivity {
         listChildData.put(listGroupTitles.get(1), userCommands);
         listChildData.put(listGroupTitles.get(2), thirdGroup);
 
-        for (ParametersItem item : themes) {
-
-        }
-
     }
+
+    public void changeTheme(String themeName) {
+        SharedPreferences prefs = getSharedPreferences("AppSettingsPrefs", MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putString("SelectedTheme", themeName);
+        editor.apply();
+
+        // Redémarrer l'activité pour appliquer le thème
+        recreate();
+    }
+
+    private void applySelectedTheme() {
+        SharedPreferences prefs = getSharedPreferences("AppSettingsPrefs", MODE_PRIVATE);
+        String themeName = prefs.getString("SelectedTheme", "BaseTheme"); // "BaseTheme" est le thème par défaut
+
+        Log.d("ThemeApply", "Applying theme: " + themeName);
+        if (themeName.equals("LightTheme")) {
+            setTheme(R.style.LightTheme);
+        } else if (themeName.equals("DarkTheme")) {
+            setTheme(R.style.DarkTheme);
+        } else {
+            setTheme(R.style.BaseTheme);
+        }
+    }
+
+    @Override
+    public void onThemeChanged(String themeName) {
+        Log.d("ThemeChange", "Changing theme to: " + themeName);
+        changeTheme(themeName);
+    }
+
 }
--- app/src/main/java/com/dcac/filesexplorer/ParametersActivityExpandableListAdapter.java
+++ app/src/main/java/com/dcac/filesexplorer/ParametersActivityExpandableListAdapter.java
@@ -18,11 +18,18 @@ public class ParametersActivityExpandableListAdapter extends BaseExpandableListA
     private List<String> listGroupTitles; // Les titres des groupes
     private HashMap<String, List<ParametersItem>> listChildData; // Les données pour les enfants, mappées par groupe
 
+    public interface OnThemeChangeListener {
+        void onThemeChanged(String themeName);
+    }
+
+    private OnThemeChangeListener onThemeChangeListener;
+
     public ParametersActivityExpandableListAdapter(Context context, List<String> listGroupTitles,
-                                                   HashMap<String, List<ParametersItem>> listChildData) {
+                                                   HashMap<String, List<ParametersItem>> listChildData, OnThemeChangeListener onThemeChangeListener) {
         this.context = context;
         this.listGroupTitles = listGroupTitles;
         this.listChildData = listChildData;
+        this.onThemeChangeListener = onThemeChangeListener;
     }
 
 
@@ -73,23 +80,68 @@ public class ParametersActivityExpandableListAdapter extends BaseExpandableListA
         return convertView;
     }
 
+
+    static class ViewHolder {
+        TextView tvChild;
+        TextView tvChildDesc;
+        CheckBox checkBox;
+    }
     @Override
     public View getChildView(int groupPosition, int childPosition, boolean isLastChild, View convertView, ViewGroup parent) {
+        ViewHolder holder;
+
         final ParametersItem childItem = (ParametersItem) getChild(groupPosition, childPosition);
+
         if (convertView == null) {
+            holder = new ViewHolder();
             LayoutInflater inflater = (LayoutInflater) this.context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
             convertView = inflater.inflate(R.layout.parameters_onglets_elements, parent, false);
+            holder.tvChild = convertView.findViewById(R.id.tvChild);
+            holder.tvChildDesc = convertView.findViewById(R.id.tvChildDesc);
+            holder.checkBox = convertView.findViewById(R.id.checkBox);
+            convertView.setTag(holder);
+        } else {
+            holder = (ViewHolder) convertView.getTag();
         }
-        TextView tvChild = convertView.findViewById(R.id.tvChild);
-        TextView tvChildDesc = convertView.findViewById(R.id.tvChildDesc);
-        CheckBox checkBox = convertView.findViewById(R.id.checkBox);
+        holder.tvChild.setText(childItem.getTitle());
+        holder.tvChildDesc.setText(childItem.getDescription());
+        holder.checkBox.setChecked(childItem.isChecked());
 
-        tvChild.setText(childItem.getTitle());
-        tvChildDesc.setText(childItem.getDescription());
-        checkBox.setChecked(childItem.isChecked());
-        // Ajouter des gestionnaires pour le CheckBox si nécessaire
+        //Ajout de l'ecouteur de clics sur la Checkbox
+        holder.checkBox.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // Décocher toutes les autres CheckBox du groupe
+                for (int i = 0; i < getChildrenCount(groupPosition); i++) {
+                    ParametersItem item = listChildData.get(listGroupTitles.get(groupPosition)).get(i);
+                    item.setChecked(false);
+                }
+
+                // Cocher la CheckBox actuelle
+                childItem.setChecked(true);
+                notifyDataSetChanged(); // Rafraîchir la vue
+
+                if (groupPosition == 0) { // Si c'est le groupe des thèmes
+                    String themeName = null;
+                    switch (childItem.getTitle()) {
+                        case "Light theme":
+                            themeName = "LightTheme";
+                            break;
+                        case "Dark theme":
+                            themeName = "DarkTheme";
+                            break;
+                        default:
+                            themeName = "BaseTheme";
+                            break;
+                    }
+                    onThemeChangeListener.onThemeChanged(themeName);
+                }
+            }
+        });
 
         return convertView;
+
     }
 
     @Override
